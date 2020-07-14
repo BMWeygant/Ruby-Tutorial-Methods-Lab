97928bcc448e5957b359382e3d6544408240d047
BMWeygant <bobbydigital108@gmail.com>
2020-07-14 15:59:17 -0400

    Added solution.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,13 @@
 #Code your solutions in this file.
+
+def hello_world
+    "Hello, world!"
+end
+
+def draft_pick(player, team)
+    "The #{team} draft #{player}."
+end
+
+def ruby_noob(subject="Ruby")
+    "I am learning #{subject}."
+end
