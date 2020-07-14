67e2b17867b706ec13ef9debaf50636c4e8a673e
BMWeygant <bobbydigital108@gmail.com>
2020-07-14 15:50:41 -0400

    Added test cases for #hello_world, #draft_pick, & #ruby_noob.

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,5 +1,20 @@
-describe "#test_method" do
-    it "Tests the method and rspec." do
-        expect(test_method).to eq("Testing 1-2.")
+describe "#hello_world" do
+    it "method has no arguments and outputs the string 'Hello, world!'." do
+        expect(hello_world).to eq("Hello, world!")
+    end
+end
+
+describe "#draft_pick" do
+    it 'method takes two argument (player and team) and drafts that player to that team.' do
+        expect(draft_pick("Sabrina Ionescu", "New York Liberty")).to eq("The New York Liberty draft Sabrina Ionescu.")
+        expect(draft_pick("Satou Sabally", "Dallas Wings")).to eq("The Dallas Wings draft Satou Sabally.")
+        expect(draft_pick("Lauren Cox", "Indiana Fever")).to eq("The Indiana Fever draft Lauren Cox.")
+    end
+end
+
+describe '#ruby_noob' do
+    it 'method takes a default argument of "Ruby" and outputs the string "I am learning #{subject}."' do
+        expect(ruby_noob).to eq("I am learning Ruby.")
+        expect(ruby_noob("history")).to eq("I am learning history.")
     end
 end
